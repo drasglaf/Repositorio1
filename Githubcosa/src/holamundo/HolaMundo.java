7005054482757e516a6657cabb745157b8539c01
Sagüés <Sags@Sags-PC>
2014-02-03 20:01:56 +0100

    commit message 1

---
+++ Githubcosa/src/holamundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package holamundo;
+//Hecho por José Mª Sagüés
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		System.out.println("caca de vaca");
+
+	}
+
+}
